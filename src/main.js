82f0865eca115d105054bb2b8c7db80747b939cb
Valeri11-Vil <vilchisvaleria11@gmail.com>
2021-02-23 23:26:25 -0600

    add dom en main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
-// Este es el punto de entrada de tu aplicacion
+import Home from './views/pages/Home.js';
+import Login from './views/pages/Login.js';
+import Register from './views/pages/Register.js';
+import Error404 from './views/pages/Error404.js';
 
-import { myFunction } from './lib/index.js';
+import navBar from './views/components/navbar.js';
+import {parseURL} from './lib/parseURL.js';
 
-myFunction();
+const routes = {
+    '/': Home,
+    '/login': Login,
+    '/registro': Register
+};
+
+const router = async () => {
+    const header = null || document.getElementById("header_root");
+    const content = null || document.getElementById("page_root");
+
+    header.innerHTML = await navBar.render();
+    await navBar.after_render();
+
+    const { resource, id, verb } = parseURL();
+
+    const parsserdURL = 
+        (resource ? '/' + resource : '/') +
+        (id ? '/:id' : '') +
+        (verb ? '/' + verb : '');
+
+        const page = routes[parsserdURL] || Error404;
+        content.innerHTML = await page.render();
+        await page.after_render();
+    };
+
+    window.addEventListener('hashchange', router);
+    window.addEventListener('load', router);
