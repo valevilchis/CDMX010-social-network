461d7cd2725d4b518f55ef54f317f5af17ad04f3
Valeri11-Vil <vilchisvaleria11@gmail.com>
2021-04-02 21:20:25 -0600

    Vista de pagina Usuario Invitado

--- src/views/pages/Invitado.js
+++ src/views/pages/Invitado.js
@@ -1,27 +1,26 @@
 const Register = {
     render: async () => {
       return  `
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Tévi</a>
+      </nav>
       <div class="container p-4">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Tévi</a>
-          <button class="btn btn-primary center" id="btn-save-post">Ver Publicaciones</button>
-        </nav>
         <div class="row">
           <div class="col-md-6">
-            <div class="card p-4">
+            <div class="card">
               <div class="card-body">
-
-                <form id="post-form">
-                  <div class="card border-info mb-3" style="max-width: 20rem;" id="post-container">
+                <form id="form-publicaciones">
+                  <div class="form-group">
+                    <textarea id="publicacion-descripcion" rows="3" class="form-control" placeholder="¿Qué nos quieres compartir?" autofocus></textarea>
                   </div>
-                  <!-- <div class="col-md-6" id="post-container"></div> -->
+                  <button class="btn btn-success" id="btn-publicar">Publicar</button>
                 </form>
-
               </div>
             </div>
           </div>
-          </div>
+          <div class="col-md-6" id="container-publicaciones"></div>
         </div>
+      </div>
       `;
     },
     after_render: async () => {}
